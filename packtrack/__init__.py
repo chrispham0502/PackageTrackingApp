242a582412398cbc864a63d3cdc8abfcd5fac89d
chrispham0502 <trietpham0502@gmail.com>
2022-05-11 13:32:23 -0400

    Added session and rewrote index route

--- packtrack/__init__.py
+++ packtrack/__init__.py
@@ -5,3 +5,4 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///packtrack.db'
 db = SQLAlchemy(app)
 
+app.secret_key = 'PACK_TRACK'
--- packtrack/routes.py
+++ packtrack/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, url_for, flash, redirect, request, Response
+import email
+from flask import render_template, url_for, flash, redirect, request, Response, session
 from packtrack import app
 from packtrack import db
 from packtrack import methods
@@ -8,27 +9,40 @@ import json
 
 @app.route("/", methods = ['POST', 'GET'])
 def index():
+
   title = "Test App"
   if request.method == "POST":
-    carrier = request.form['carrier']
-    tracking_number = request.form['trackingNum']
+
+    state = request.form['btn_identifier']
+
+    if state == 'track_package':
+      session['carrier'] = request.form['carrier']
+      session['tracking_number'] = request.form['trackingNum']
+
+      events = methods.getTrackingEvents(session['carrier'], session['tracking_number'])
     
-    package = methods.getPackageByTrackingNumber(tracking_number)
+      return render_template("index.html", title = title, events = events, state = state)
 
-    # If package doesn't exist in database yet, process new package
-    if package is None:
-      title = "New Package"
-      package = methods.processPackage(carrier, tracking_number)
-
-    # If already existed then proceed
     else:
-      title = "Old Package"
+      session['package_name'] = request.form['name']
+      session['package_description'] = request.form['description']
+      session['email'] = request.form['email']
 
-    events = package.events
-    return render_template("index.html", title = title, events = events)
+      new_user = methods.processUser(session['email'])
+
+      new_package = methods.processPackage(session['carrier'], session['tracking_number'], session['package_name'], session['package_description'] )
+
+      new_package.users.append(new_user)
+      db.session.commit()
+
+      methods.subscribePackage(session['carrier'], session['tracking_number'])
+      
+      return "Sending info of package " + session['package_name'] + session['package_description'] + session['tracking_number'] + " to: " +  session['email']
 
   else:
-    return render_template("index.html", title = title)
+    db.drop_all()
+    db.create_all()
+    return render_template("index.html", title = title, state = 'load')
 
 
 # Webhook Handler
