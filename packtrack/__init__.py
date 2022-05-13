597abcd0fe484be608e4062d2c63ffe95f39389d
chrispham0502 <trietpham0502@gmail.com>
2022-05-13 00:55:55 -0400

    Added more environmental var

--- packtrack/__init__.py
+++ packtrack/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -5,4 +6,4 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///packtrack.db'
 db = SQLAlchemy(app)
 
-app.secret_key = 'PACK_TRACK'
+app.secret_key = os.getenv("SECRET_KEY")
