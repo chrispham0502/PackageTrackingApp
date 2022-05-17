0bad08c5874964a6b190e9f2d88f28a732d82a8a
chrispham0502 <trietpham0502@gmail.com>
2022-05-17 00:53:30 -0400

    Added phone port

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from packtrack import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-from packtrack import routes
+    app.run(debug=True, host="0.0.0.0")
--- packtrack/__init__.py
+++ packtrack/__init__.py
@@ -7,3 +7,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///packtrack.db'
 db = SQLAlchemy(app)
 
 app.secret_key = os.getenv("SECRET_KEY")
+
+from packtrack import routes
