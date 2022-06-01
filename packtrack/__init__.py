3fcc87f956195180c09b7d1b8ff08241450725c3
chrispham0502 <trietpham0502@gmail.com>
2022-06-01 18:01:35 -0400

    Added Heroku database

--- packtrack/__init__.py
+++ packtrack/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///packtrack.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///packtrack.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://shhegbwfloylkc:ec9cd3e626bf2ca4122093da55ead04cf26d94058883706eff51c230f074f29a@ec2-34-227-120-79.compute-1.amazonaws.com:5432/dc7qnifqan4ln0'
 db = SQLAlchemy(app)
 
 app.secret_key = os.getenv("SECRET_KEY")
