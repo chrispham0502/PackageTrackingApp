8ef019a9668c2987e61c9731e2f95f394d23afe9
chrispham0502 <trietpham0502@gmail.com>
2022-06-01 18:07:53 -0400

    Updated db uri

--- packtrack/__init__.py
+++ packtrack/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///packtrack.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://shhegbwfloylkc:ec9cd3e626bf2ca4122093da55ead04cf26d94058883706eff51c230f074f29a@ec2-34-227-120-79.compute-1.amazonaws.com:5432/dc7qnifqan4ln0'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://shhegbwfloylkc:ec9cd3e626bf2ca4122093da55ead04cf26d94058883706eff51c230f074f29a@ec2-34-227-120-79.compute-1.amazonaws.com:5432/dc7qnifqan4ln0'
 db = SQLAlchemy(app)
 
 app.secret_key = os.getenv("SECRET_KEY")
