499b8f3bb89a953cd2d5fc7c8ef7ed45f978a9f1
chrispham0502 <chris.pham@farental.com>
2024-02-02 19:26:45 -0500

    Change sqlitedb

--- packtrack/__init__.py
+++ packtrack/__init__.py
@@ -3,13 +3,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///packtrack.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///packtrack.db'
 
+#dbURL = os.getenv('DB_URL')
+#app.config['SQLALCHEMY_DATABASE_URI'] = dbURL
 
-
-dbURL = os.getenv('DB_URL')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = dbURL
 db = SQLAlchemy(app)
 
 app.secret_key = os.getenv("SECRET_KEY")
